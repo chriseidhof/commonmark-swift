97d037b5623b7c8cb08dace4add67b360c458428
Chris Eidhof <chris@eidhof.nl>
2015-07-17 10:00:49 +0200

    Initial Commit

---
+++ CommonMark/CommonMark.h
@@ -0,0 +1,19 @@
+//
+//  CommonMark.h
+//  CommonMark
+//
+//  Created by Chris Eidhof on 17/07/15.
+//  Copyright © 2015 Chris Eidhof. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CommonMark.
+FOUNDATION_EXPORT double CommonMarkVersionNumber;
+
+//! Project version string for CommonMark.
+FOUNDATION_EXPORT const unsigned char CommonMarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonMark/PublicHeader.h>
+
+
