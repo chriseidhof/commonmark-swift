241803cc4b706fcc04b6a22fd3470f0178bbd7e6
Chris Eidhof <chris@eidhof.nl>
2015-07-17 14:04:25 +0200

    Header file cleanup

--- CommonMark/CommonMark.h
+++ CommonMark/CommonMark.h
@@ -14,7 +14,4 @@ FOUNDATION_EXPORT double CommonMarkVersionNumber;
 //! Project version string for CommonMark.
 FOUNDATION_EXPORT const unsigned char CommonMarkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CommonMark/PublicHeader.h>
-
-
 #import <cmark/cmark.h>
